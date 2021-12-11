e85693a19e37ee5edbe1bf4064014c67c293a83c
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-12-12 00:57:05 +0600

    'first-commit'

---
+++ pages/news/index.js
@@ -0,0 +1,8 @@
+import Link from "next/link"
+const News = () => {
+    return (<>
+        <h2>Hellow news readers</h2>
+        <li><Link href = '/news/sanzida love'>navigate</Link></li>
+    </>)
+}
+export default News
