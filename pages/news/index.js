aeee65c17d9232324a60a637972fdd9f42751e84
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-12-15 11:06:59 +0600

    'good'

--- pages/news/index.js
+++ pages/news/index.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 const News = () => {
     return (<>
         <h2>Hellow news readers</h2>
-        <li><Link href = '/news/sanzida love'>navigate</Link></li>
+        <li><Link href = '/news/hello'>navigate</Link></li>
     </>)
 }
 export default News
