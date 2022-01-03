d8a7804830fa92eb575966212b8d356ad6589115
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-03 21:40:41 +0600

    'deleted-news'

--- pages/news/[newsId].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router"
-const News = () => {
-    const router = useRouter()
-    return (<>
-        <h2>Hellow news readers it is detailed {router.query.newsId}</h2>
-    </>)
-}
-export default News
--- pages/news/index.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-import Layout from "../../components/Layout"
-const News = () => {
-    return (<Layout>
-        <h2>Hellow news readers</h2>
-        <li><Link href = '/news/hello'>navigate</Link></li>
-    </Layout>)
-}
-export default News
