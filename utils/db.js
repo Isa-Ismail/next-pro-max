fbcbf42752546635d7a0f99d72aa9a1a2f38b8de
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-04 14:22:23 +0600

    'new-conect'

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,47 @@
 import mongoose from 'mongoose';
 
-const connectDB = handler => async (req, res) => {
-  if (mongoose.connections[0].readyState) {
-    // Use current db connection
-    return handler(req, res);
-  }
-  // Use new db connection
-  await mongoose.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true
-  });
-  return handler(req, res);
-};
+const connection = {};
 
-export default connectDB;
+const connect = async () => {
+  if (connection.isConnected) {
+    console.log('already connected');
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log('use previous connection');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log('new connection');
+  connection.isConnected = db.connections[0].readyState;
+}
+
+const disconnect = async () => {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log('not disconnected');
+    }
+  }
+}
+
+function convertDocToObj(doc) {
+  doc._id = doc._id.toString();
+  doc.createdAt = doc.createdAt.toString();
+  doc.updatedAt = doc.updatedAt.toString();
+  return doc;
+}
+
+const db = { connect, disconnect, convertDocToObj };
+
+export default db;
