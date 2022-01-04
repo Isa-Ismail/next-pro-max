d0126094e5894308de5d03756bebd0bd9a66ccf6
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-04 14:16:25 +0600

    'mongo'

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    mongodburl: 
+      "mongodb+srv://fahim:fahimismail75@cluster0.tkgtp.mongodb.net/next-nosql?retryWrites=true&w=majority",
+}
 }
