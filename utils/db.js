c4d3d80134f23471f0eefcd2b961828c1671da80
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-08 13:45:29 +0600

    'databasae'

--- utils/db.js
+++ utils/db.js
@@ -17,8 +17,7 @@ const connect = async () => {
   }
   const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+    useUnifiedTopology: true
   });
   console.log('new connection');
   connection.isConnected = db.connections[0].readyState;
