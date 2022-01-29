0c76da43987535582db478a77938cecc28c35e98
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-29 21:21:45 +0600

    '[id]-fix_req.query.id'

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,14 +1,14 @@
-import nc from 'next-connect'
-import Product from '../../../models/Product'
-import db from '../../../utils/db'
+import nc from 'next-connect';
+import Product from '../../../models/Product';
+import db from '../../../utils/db';
 
-const handler = nc()
+const handler = nc();
 
-handler.get( async (req, res) => {
-    await db.connect()
-    const product = Product.findById(req.params.id)
-    await db.disconnect()
-    res.json(product)
-})
+handler.get(async (req, res) => {
+  await db.connect();
+  const product = await Product.findOne ( { _id: req.query.id } );
+  await db.disconnect();
+  res.send(product);
+});
 
-export default handler
+export default handler;
