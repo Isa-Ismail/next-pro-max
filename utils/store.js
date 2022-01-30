9d8e7bc6375fee917031384443ea7c7ed597b46c
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-30 14:11:12 +0600

    'cart-fix'

--- utils/store.js
+++ utils/store.js
@@ -5,8 +5,9 @@ export const Store = createContext()
 
 const initialState = {
     darkMode: Cookies.get('darkMode') === 'ON',
-    cart:{
-        cartItems:[]
+
+    cart: {
+        cartItems: Cookies.get('cartItems') ? JSON.parse(Cookies.get('cartItems')) : []
     }
 }
 
@@ -19,16 +20,16 @@ const reducer = (state, action) => {
                 }
             case 'CART_ADD_ITEM': {
                 const newItem = action.payload;
-                const existItem = state.cart.cartItems.find(
-                  (item) => item._id === newItem._id
-                );
-                const cartItems = existItem
-                  ? state.cart.cartItems.map((item) =>
-                      item.name === existItem.name ? newItem : item
-                    )
-                  : [...state.cart.cartItems, newItem];
-                Cookies.set('cartItems', JSON.stringify(cartItems));
-                return { ...state, cart: { ...state.cart, cartItems } };
+                
+                const existItem = state.cart.cartItems.find( (item) => item._id === newItem._id)
+                
+                const cartItems = existItem ? 
+                state.cart.cartItems.map((item) => item.name === existItem.name ? newItem : item )
+                : [...state.cart.cartItems, newItem];
+                
+                Cookies.set('cartItems', JSON.stringify(cartItems))
+                
+                return { ...state, cart: { ...state.cart, cartItems } }
               }
         default:
             return state
