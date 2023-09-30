4b0b9d777a687d2826bdda401eb3f31f788c65b7
ismail <62422922+Isa-Ismail@users.noreply.github.com>
2023-09-30 13:30:06 +0600

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
     minheight: '100vh',
   },
   image: {
-    backgroundImage: 'url(images/attack.jpg)',
+    backgroundImage: 'url(images/attack.png)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
