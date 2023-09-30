cb8844f255716c364290de544b4ca5e60bc0f365
ismail <62422922+Isa-Ismail@users.noreply.github.com>
2023-09-30 13:29:53 +0600

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(images/attack.jpg)',
+    backgroundImage: 'url(images/attack.png)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
